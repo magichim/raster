be71d84339c8f0bcb31a7897dc66a2f3d0ca1040
magichim <songwon.bak@gmail.com>
2018-03-20 11:39:13 +0900

    * mobile_check -> device_check

--- js/lib/device_check.js
+++ js/lib/device_check.js
@@ -1,4 +1,4 @@
-var mobile_check = function () {
+var device_check = function () {
   navigator.userAgent.match(/Android|iPhone|iPod|IEMobile|Opera Mini|BlackBerry/i) ?
     alert('Mobile phone can\'t use this site.') : true;
 };
